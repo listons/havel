3a7fc4b3a82ea8a1752e06aaa28b63810a4bce03
Anon2013 <acastoiljkovic997@gmail.com>
2017-02-26 19:15:22 +0100

    Add files via upload

---
+++ ScriptPrikazProizvoda.js
@@ -0,0 +1,11 @@
+function prikaziT(n){
+		//setTimeout(function(){
+			document.getElementById(n).style.display = 'block';
+   //}, 2000);
+}
+function prikazi(n){
+		document.getElementById(n).style.display = 'block';
+}
+function sakri(n){
+		document.getElementById(n).style.display = 'none';
+}
