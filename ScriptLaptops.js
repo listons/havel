9994a5fb397916a025c8332b344d244977afd69a
Anon2013 <acastoiljkovic997@gmail.com>
2017-03-13 23:12:23 +0100

    Add files via upload

--- ScriptLaptops.js
+++ ScriptLaptops.js
@@ -114,6 +114,7 @@ function prikaziCetvrtuStranu(){ //Prikazuje proizvode sa cetvrte stranice
         document.getElementById("50").style.display = 'block';
 }
 function sort(value) {
+    
     if (value == '2') { //Sortira po opadajucem redosledu
 	var c = 0;
     var pom;
@@ -178,6 +179,49 @@ else if (value == '1') { //Sortira po rastucem redosledu
         for(var j=0; j<laptops.length-1; j++)
         {
             if(laptops[j].cena > laptops[j+1].cena)
+            {
+                pom = laptops[j];
+                laptops[j] = laptops[j+1];
+                laptops[j+1] = pom;
+            }
+        }
+    }
+    /*for(var m = 0; m<laptops.length; m++)
+    {
+    sve2+=laptops[m].cena;
+    }
+    alert(sve2);*/
+    c=0;
+     for (var i = 1; i < num.length; i+=2) {//Ispis
+        for (var j = c; j <i-c; j++) {
+            num[i].childNodes[1].innerHTML = laptops[j].slika;
+            num[i].childNodes[3].innerHTML = laptops[j].ime;
+            num[i].childNodes[5].innerHTML = laptops[j].cena;
+        }   
+        c++;
+    }
+}
+else if (value == '3') { //Sortira po brendu
+    var c = 0;
+    var pom;
+    var sve1,sve2;
+    var num = document.getElementById('glavni').childNodes;
+    for (var i = 1; i < num.length; i+=2) {
+        for (var j = c; j <i-c; j++) {
+            laptops[j] = { slika:num[i].childNodes[1].innerHTML, ime:num[i].childNodes[3].innerHTML, cena:num[i].childNodes[5].innerHTML};
+        }   
+        c++;
+    }
+    /*for(var m = 0; m<laptops.length; m++)
+    {
+    sve1+= laptops[m].cena;
+    }
+    alert(sve1);*/
+    for(var i = 0; i<laptops.length; i++)//Sortiranje
+    {
+        for(var j=0; j<laptops.length-1; j++)
+        {
+            if(laptops[j].ime > laptops[j+1].ime)
             {
                 pom = laptops[j];
                 laptops[j] = laptops[j+1];
